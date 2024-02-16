042d81dc82bf334a242f21aadb4751cd3292b064
Felipe Ferraz <felipeferraz1398@gmail.com>
2024-02-16 16:56:15 -0300

    Inserting pokemons dynamically with js and api

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const pokemonList = document.getElementById('pokemonList')
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="types ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml 
+    })
+  
+
